7391148cb88b7ed1184a238cb12d06a22cdd752a
Lucas Kim <pointbre@gmail.com>
2023-11-24 01:10:00 +1300

    saved

--- asyncer-core/src/main/java/com/github/pointbre/asyncer/core/Asyncer.java
+++ asyncer-core/src/main/java/com/github/pointbre/asyncer/core/Asyncer.java
@@ -2,7 +2,6 @@ package com.github.pointbre.asyncer.core;
 
 import java.time.Instant;
 import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.Callable;
 import java.util.concurrent.StructuredTaskScope;
 
